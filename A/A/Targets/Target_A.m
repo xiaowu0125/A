97fa5802c04c74002948982a682ed51c203c1178
fansen <18037860536@163.com>
2021-01-04 11:07:57 +0800

    新增block回调

--- A/A/Targets/Target_A.m
+++ A/A/Targets/Target_A.m
@@ -17,4 +17,16 @@
     return viewController;
 }
 
+- (UIViewController *)Action_Category_ViewController:(NSDictionary *)params {
+    
+    typedef void (^CallbackType)(NSString *);
+    CallbackType callback = params[@"callback"];
+    if (callback) {
+        callback(@"success");
+    }
+    
+    AViewController *viewController = [[AViewController alloc] init];
+    return viewController;
+}
+
 @end
